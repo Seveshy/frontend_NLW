b7c14fbb323b4e6952d207bb71bb85338132ba87
Seveshy <danielalvesmajor@outlook.com>
2020-11-02 10:47:37 -0300

    Navegação entre as telas

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
 import OrphanangesMap from './pages/OrphanangesMap'
+import Orphanage from './pages/Orphanage'
+import CreateOrphanage from './pages/CreateOrphanage'
 
 function Routes() {
     return (
@@ -10,6 +12,10 @@ function Routes() {
            <Switch>
             <Route exact path="/" component={Landing} />
             <Route path="/app" component={OrphanangesMap} />
+           
+            <Route path="/orphanages/create" component={CreateOrphanage} />
+            <Route path="/orphanages/:id" component={Orphanage} />
+           
            </Switch>
         </BrowserRouter>
     )
