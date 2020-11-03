def742013728eeb1a9c4ee4812358dae7082b95d
Seveshy <danielalvesmajor@outlook.com>
2020-11-03 11:25:28 -0300

    Connect api

--- src/pages/OrphanangesMap.tsx
+++ src/pages/OrphanangesMap.tsx
@@ -2,21 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 
+import mapIcon from '../utils/mapIcon'
+
 import {  Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import Leaflet from 'leaflet'
 
 import mapMarkerImg from '../images/map-marker.png'
 
 import '../styles/pages/orphanages-map.css'
 
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
 
 function OrphanangesMap() {
     return (
